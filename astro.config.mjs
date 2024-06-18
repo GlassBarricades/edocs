7cd148e3cddf57fd5da90045d377408b845a8cee
Glass Barricades <glassbarricades1@list.ru>
2024-06-18 05:47:02 +0300

    start project

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Емеля",
+	  locales: {
+		root: {
+			label: 'Русский',
+			lang: 'ru'
+		}
+	  },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Инструкции для водителей",
+          autogenerate: { directory: "drivers" },
+        },
+        {
+          label: "Инструкции для операторов",
+          autogenerate: { directory: "operators" },
+        },
+      ],
+    }),
+  ],
+});
---
+++ src/content/docs/drivers/application.md
@@ -0,0 +1,13 @@
+---
+title: Водительское приложение
+description: Подробная инструкция по работе с водительским приложением.
+---
+
+Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
+Writing a good guide requires thinking about what your users are trying to do.
+
+![image](https://monsterface.ru/wp-content/uploads/2021/09/ghost_rider-1-850x482.jpg)
+
+## Further reading
+
+- Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework
