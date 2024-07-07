866c314e686e4d12fc221cc03faea948650be73a
Glass Barricades <glassbarricades1@list.ru>
2024-07-07 16:52:34 +0300

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,36 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Емеля",
-	  locales: {
-		root: {
-			label: 'Русский',
-			lang: 'ru'
-		}
-	  },
-      social: {
-        github: "https://github.com/withastro/starlight",
-      },
-      sidebar: [
-        {
-          label: "Инструкции для водителей",
-          autogenerate: { directory: "drivers" },
-        },
-        {
-          label: "Инструкции для операторов",
-          autogenerate: { directory: "operators" },
-        },
-        {
-          label: "Общее",
-          autogenerate: { directory: "all" },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: "Емеля",
+    locales: {
+      root: {
+        label: 'Русский',
+        lang: 'ru'
+      }
+    },
+    social: {
+      github: "https://github.com/withastro/starlight"
+    },
+    sidebar: [{
+      label: "Инструкции для водителей",
+      autogenerate: {
+        directory: "drivers"
+      }
+    }, {
+      label: "Инструкции для операторов",
+      autogenerate: {
+        directory: "operators"
+      }
+    }, {
+      label: "Общее",
+      autogenerate: {
+        directory: "all"
+      }
+    }]
+  }), tailwind()]
 });
